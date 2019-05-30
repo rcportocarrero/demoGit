5f8daf39d200cc5b0f0d21032b42382800291d89
Roy Cespedes <roy.cespedes@mdp.com.pe>
2019-05-30 10:15:46 -0500

    fx

---
+++ PCS1/DonnyScripts.sql
@@ -0,0 +1,7 @@
+-- call newUser('78887845','Roy','Cespedes','Portocarrero','rc@gm.com','987351755','45381934',2,'$2y$10$3jYy0LMyT2XlgoxeBxqfFeCH6TTRUKkZJEVAM2BsSmQN2Oi6g0ES6')
+
+
+/*'$2y$10$3jYy0LMyT2XlgoxeBxqfFeCH6TTRUKkZJEVAM2BsSmQN2Oi6g0ES6'*/
+-- truncate table tb_service;
+
+select id_referido from tb_cliente where id_cliente=2 and num_estado=1;
---
+++ ee/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.clearHistory();
+        myWebView.clearFormData();
+        myWebView.clearCache(true);
+        myWebView.loadUrl("http://tiempocreativo.pe/demopage/examples/register-page.html");
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        WebView mWebView;
+        mWebView = (WebView) findViewById(R.id.webView);
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (mWebView.canGoBack()) {
+                        mWebView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
